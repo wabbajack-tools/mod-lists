5d67c99a6102d32a74a642240236382d71193f79
erri120 <erri120@protonmail.com>
2020-01-19 13:22:02 +0100

    Added dead link checker

--- modlists.test.js
+++ modlists.test.js
@@ -1,4 +1,6 @@
 const modlists = require('./modlists.json');
+const _ = require('underscore');
+const axios = require('axios').default;
 
 test('parse JSON', () => {
     const data = JSON.stringify(modlists);
@@ -8,4 +10,23 @@ test('parse JSON', () => {
     expect(s).not.toBeNull();
     expect(s).not.toBeUndefined();
     expect(data).toEqual(JSON.stringify(s));
+});
+
+const checkLink = link => {
+    axios.get(link).then(res => res.status).catch(err => err);
+}
+
+describe(`checking links for ${modlists.length} Modlists`, () => {
+    _.map(modlists, modlist => {
+        _.map(modlist.links, link => {
+            if(!link.startsWith("https://") && !link.startsWith("http://")) return;
+            
+            test(`checking link ${link} of ${modlist.title}`, async () => {
+                expect.assertions(1);
+                return axios.get(link).then(res => {
+                    expect(res.status).toBe(200);
+                }).catch(err => err);
+            })
+        });
+    });
 });
