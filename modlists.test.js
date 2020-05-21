5d968b7bfff6ca4e95b02c67b6bdb0b0c9e85523
erri120 <erri120@protonmail.com>
2020-05-21 18:47:23 +0200

    Updated unit test

--- modlists.test.js
+++ modlists.test.js
@@ -1,12 +1,36 @@
 const modlists = require('./modlists.json');
 const _ = require('underscore');
 
-test('parse JSON', () => {
-    const data = JSON.stringify(modlists);
-    const s = JSON.parse(data);
-    expect(data).not.toBeNull();
-    expect(data).not.toBeUndefined();
-    expect(s).not.toBeNull();
-    expect(s).not.toBeUndefined();
-    expect(data).toEqual(JSON.stringify(s));
+describe('test modlists.json', () => {
+    it('parses correctly', () => {
+        const data = JSON.stringify(modlists);
+        const s = JSON.parse(data);
+        expect(data).not.toBeNull();
+        expect(data).not.toBeUndefined();
+        expect(s).not.toBeNull();
+        expect(s).not.toBeUndefined();
+        expect(data).toEqual(JSON.stringify(s));
+    });
+
+    const regex  = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    const testLink = (link) => {
+        const success = regex.test(link);
+        if(!success)
+            throw `Regex doesn't match for ${link}`;
+        return success;
+    }
+    it('has no URL errors', () => {
+        _.forEach(modlists, modlist => {
+            expect(testLink(encodeURI(modlist.links.image))).toBeTruthy();
+            expect(testLink(encodeURI(modlist.links.download))).toBeTruthy();
+            expect(testLink(encodeURI(modlist.links.readme))).toBeTruthy();
+
+            expect(modlist.links.machineURL.includes(' ')).toBeFalsy();
+        });
+    });
 });
