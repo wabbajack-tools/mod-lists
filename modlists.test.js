bccc6c3c8c0cab0f5f5d7d810e5c02725dc97291
erri120 <erri120@protonmail.com>
2020-05-24 12:48:47 +0200

    Updated test

--- modlists.test.js
+++ modlists.test.js
@@ -15,11 +15,11 @@ describe('test modlists.json', () => {
     const regex  = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+\']*)*'+ // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
     const testLink = (link) => {
-        const success = regex.test(link);
+        const success = regex.test(link.toLowerCase());
         if(!success)
             throw `Regex doesn't match for ${link}`;
         return success;
